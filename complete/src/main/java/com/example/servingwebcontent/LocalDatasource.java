0fa688f772748e7e183958d848a90e2a83caebc3
tranlq91 <tranlq91@gmail.com>
2024-08-12 15:41:27 +0700

    initial project

---
+++ complete/src/main/java/com/example/servingwebcontent/LocalDatasource.java
@@ -0,0 +1,11 @@
+package com.example.servingwebcontent;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LocalDatasource {
+    private String url;
+}
+
---
+++ complete/src/main/java/com/example/servingwebcontent/Message.java
@@ -0,0 +1,6 @@
+package com.example.servingwebcontent;
+
+/**
+ * Simple domain object for our API to return a message.
+ */
+public record Message(String message) {}
